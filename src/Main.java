8113b2134c55402d313f57e1efca7d4f218f0e84
zainisacoder <zain.hanif88@gmail.com>
2022-11-23 09:53:07 +0100

    added feature1 println

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello again");
-
+        System.out.println("this is from feature1");
+        
     }
 }
