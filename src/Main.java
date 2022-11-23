cee9e5ae4659a90d2163b43ddd9f6f4ee268bd6c
zainisacoder <zain.hanif88@gmail.com>
2022-11-23 10:32:27 +0100

    its a conflict

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("its a conflict");
         System.out.println("Hello world!");
         System.out.println("Hello again");
         System.out.println("this is from my class");
