faa7f6b90b3366627e498c70bee85bc35be4faa7
zainisacoder <zain.hanif88@gmail.com>
2022-11-23 10:17:52 +0100

    another change to main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello again");
-        System.out.println("this is from feature1");
+        System.out.println("this is from my class");
 
     }
 }
