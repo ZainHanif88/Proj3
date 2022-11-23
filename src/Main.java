da84ffc3abcef20eecf97951197f3581f1b9b0d5
zainisacoder <zain.hanif88@gmail.com>
2022-11-23 10:29:31 +0100

    this is from feature1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("this is from feature1");
         System.out.println("Hello world!");
         System.out.println("Hello again");
         System.out.println("this is from my class");
